125445f5e42b19cf89b6aa91241837d0d1a2bcf9
Lin Jie <ljlovemichael@hotmail.com>
2016-03-25 21:59:19 +0800

    sqlitepcl

---
+++ Sqlitelearn/SQLboomlitepcl/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SQLitePCL;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SQLboomlitepcl
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private static String DB_NAME = "SQLiteSample.db";
+        private static String TABLE_NAME = "SampleTable";
+        private static String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (Key TEXT,Value TEXT);";
+        private static String SQL_QUERY_VALUE = "SELECT * FROM " + TABLE_NAME;// + " WHERE Key = (?);";
+        private static String SQL_INSERT = "INSERT INTO " + TABLE_NAME + " VALUES(?,?);";
+        private static String SQL_UPDATE = "UPDATE " + TABLE_NAME + " SET Value = ? WHERE Key = ?";
+        private static String SQL_DELETE = "DELETE FROM " + TABLE_NAME + " WHERE Key = ?";
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void create_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteConnection conn = new SQLiteConnection(DB_NAME);
+            using (var statement = conn.Prepare(SQL_CREATE_TABLE))
+            {
+                statement.Step();
+            }
+        }
+        private void insert_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteConnection conn = new SQLiteConnection(DB_NAME);
+            using (var statement = conn.Prepare(SQL_INSERT))
+            {
+                statement.Bind(1, "hhhhh");
+                statement.Bind(2, "fkfjfk");
+                statement.Step();
+            }
+        }
+
+        private void read_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteConnection conn = new SQLiteConnection(DB_NAME);
+            using (var statement = conn.Prepare(SQL_QUERY_VALUE))
+            {
+               var a= statement.Step();
+                if (SQLiteResult.ROW == a)
+                {
+                   var  value = statement[0] as String;
+                }
+
+            }
+        }
+
+        private void update_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void date_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
--- Sqlitelearn/sqlite0/MainPage.xaml.cs
+++ Sqlitelearn/sqlite0/MainPage.xaml.cs
@@ -18,7 +18,7 @@ using Windows.Storage;
 using sqlite0.Models;
 using System.Text;
 using Windows.UI.Popups;
-
+using SQLite;
 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -31,12 +31,8 @@ namespace sqlite0
     public sealed partial class MainPage : Page
     {
         private static String DB_NAME = "SQLiteSample.db";
-        private static String TABLE_NAME = "SampleTable";
-        private static String SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (Key TEXT,Value TEXT);";
-        private static String SQL_QUERY_VALUE = "SELECT Value FROM " + TABLE_NAME + " WHERE Key = (?);";
-        private static String SQL_INSERT = "INSERT INTO " + TABLE_NAME + " VALUES(?,?);";
-        private static String SQL_UPDATE = "UPDATE " + TABLE_NAME + " SET Value = ? WHERE Key = ?";
-        private static String SQL_DELETE = "DELETE FROM " + TABLE_NAME + " WHERE Key = ?";
+
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -58,6 +54,7 @@ namespace sqlite0
                 conn.InsertAll(listd);
             }
         }
+
         private async void Button_Click1(object sender, RoutedEventArgs e)
         {
             string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, DB_NAME);
@@ -74,7 +71,7 @@ namespace sqlite0
             }
         }
 
-        private async void Button_Click2(object sender, RoutedEventArgs e)
+        private void Button_Click2(object sender, RoutedEventArgs e)
         {
             string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, DB_NAME);
             using (var conn = new SQLiteConnection(new SQLitePlatformWinRT(), path))
@@ -82,25 +79,39 @@ namespace sqlite0
                 conn.CreateTable<User>();
                 //conn.Update(new User { Id = 1, UserName = "Michael Jackson" });//垃圾
 
-                var q = conn.Table<User>().Where(x => x.UserName == "Lin").FirstOrDefault();
+                var q = conn.Table<User>().FirstOrDefault(x => x.UserName == "Lin");
                 if (q != null)
                 {
                     q.UserName = "Michael";
                     conn.Update(q);
                 }
-
-
             }
         }
 
-        //public static SQLiteConnection GetDbConnection()
-        //{
-        //public readonly static string DbPath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "test.db");
-        //// 连接数据库，如果数据库文件不存在则创建一个空数据库。
-        //var conn = new SQLiteConnection(new SQLitePlatformWinRT(), DbPath);
-        //// 创建 Person 模型对应的表，如果已存在，则忽略该操作。
-        //conn.CreateTable<Person>();
-        //    return conn;
-        //}
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, DB_NAME);
+            using (var conn = new SQLiteConnection(new SQLitePlatformWinRT(), path))
+            {
+                conn.CreateTable<User>();
+                //conn.Update(new User { Id = 1, UserName = "Michael Jackson" });//垃圾
+
+                var q = conn.Table<User>().FirstOrDefault(x => x.UserName == "D");
+                if (q != null)
+                {
+                    conn.Delete(q);
+                }
+            }
+
+            //public static SQLiteConnection GetDbConnection()
+            //{
+            //public readonly static string DbPath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "test.db");
+            //// 连接数据库，如果数据库文件不存在则创建一个空数据库。
+            //var conn = new SQLiteConnection(new SQLitePlatformWinRT(), DbPath);
+            //// 创建 Person 模型对应的表，如果已存在，则忽略该操作。
+            //conn.CreateTable<Person>();
+            //    return conn;
+            //}
+        }
     }
 }
